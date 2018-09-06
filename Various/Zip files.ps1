7d23ab869dd29ed13f3c47f54034713bfa6fcae2
Jochen Mariën <jochen.marien@thomasmore.be>
2018-09-06 14:55:22 +0200

    update 7

---
+++ Various/Zip files.ps1
@@ -0,0 +1,12 @@
+﻿function Zip-Files( $zipfilename, $sourcedir )
+{
+   Add-Type -Assembly System.IO.Compression.FileSystem
+   $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+   [System.IO.Compression.ZipFile]::CreateFromDirectory($sourcedir,
+        $zipfilename, $compressionLevel, $false)
+}
+
+$dir = "C:\tmp\tmp"
+$file = "c:\tmp\tmp.zip"
+
+Zip-Files $file $dir
